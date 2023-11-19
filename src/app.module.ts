182744e25540459716a3832d28185b9348d9cc8d
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-11-19 14:06:55 -0500

    Setting environment variables and changes for db in PG

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      // ssl: process.env.POSTGRES_SSL === 'true',
+      // extra: {
+      //   ssl:
+      //     process.env.POSTGRES_SSL === 'true'
+      //       ? {
+      //           rejectUnauthorized: false,
+      //         }
+      //       : null,
+      // },
+    }),
+    // Otros módulos
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
