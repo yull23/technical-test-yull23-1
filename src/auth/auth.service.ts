6fb2769fa2ac34894677e16d4d5e3ebbbc5a7106
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-11-20 05:10:54 -0500

    Getting token with jwtService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcryptjs from 'bcryptjs';
 import { LoginUserDeto } from 'src/common/dtos/login-user.dto';
 import { UsersService } from 'src/users/users.service';
@@ -10,7 +11,10 @@ import { RegisterUserDto } from '../common/dtos/register-user.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtServices: JwtService,
+  ) {}
 
   async register(registerUserDto: RegisterUserDto) {
     const user = await this.userService.findOneByEmail(registerUserDto.email);
@@ -37,6 +41,14 @@ export class AuthService {
     if (!isPasswordValid) {
       throw new UnauthorizedException('Password invalid');
     }
-    return user;
+    const payload = { email: user.email, role: user.role };
+    const token = await this.jwtServices.signAsync(payload);
+    return {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      role: user.role,
+      token,
+    };
   }
 }
